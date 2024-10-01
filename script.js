f33338ec3e89a9e99d7995e8f3e96e29e12c83ac
raja <rashidaliwassan786@gmail.com>
2024-10-01 23:15:45 +0500

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-function showWhatsAppText() {
-            var whatsappText = document.getElementById("whatsappText");
-            whatsappText.style.display = "block";
-            setTimeout(function() {
-                window.location.href = "https://wa.me//03033032032"; // Replace with your WhatsApp number
-            }, 2000); // Delay of 2 seconds before redirecting
-        }
-        
